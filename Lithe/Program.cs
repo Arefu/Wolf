c9fd08af8d474907ce92133b61796816c36aa6ff
Johnathon <f315729@trbvm.com>
2017-07-07 17:04:50 +1200

    It worked for me...
    Added very basic program called Lithe that lets you edit credits.dat. Refer to README for more information and how to use it.

---
+++ Lithe/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Celtic_Guardian;
+
+namespace Lithe
+{
+    internal class Program
+    {
+        private static void Main(string[] Args)
+        {
+            Console.Title = "Lithe";
+            if (Args.Length <= 0)
+                Utilities.Log("Please Drag The \"credits.dat\" File On To Me!", Utilities.Event.Error, true, 1);
+            if (!Args[0].ToLower().EndsWith("credits.dat") && !Args[0].ToLower().EndsWith("credits.txt"))
+                Utilities.Log("\"credits.dat\" or \"credits.txt\" not specified!", Utilities.Event.Error, true, 1);
+
+            var Credits = Args[0];
+
+            var PackOrUnpack = Credits.EndsWith("credits.dat"); //False Pack, True Parse.
+
+            if (PackOrUnpack)
+            {
+                using (var Reader = new BinaryReader(File.Open(Credits, FileMode.Open, FileAccess.Read)))
+                {
+                    var CreditsContent = Reader.ReadBytes((int)new FileInfo(Credits).Length);
+                    File.WriteAllText("credits.txt", Utilities.GetText(CreditsContent).Replace("?", String.Empty));
+                    Utilities.Log("Finished Parsing.", Utilities.Event.Information);
+                }
+            }
+            else
+            {
+                using (var Writer = new BinaryWriter(File.Open("credits.dat", FileMode.Append, FileAccess.Write)))
+                {
+                    using (var Reader = new BinaryReader(File.Open("credits.txt", FileMode.Open, FileAccess.Read)))
+                    {
+                        var Content = Reader.ReadBytes((int)Reader.BaseStream.Length);
+                        Writer.Write(new byte[] { 0xFF, 0xFE });
+                        foreach (var Char in Encoding.ASCII.GetString(Content))
+                        {
+                            switch (Char)
+                            {
+                                case '\r':
+                                    Writer.Write(new byte[] { 0x0D, 0x00 });
+                                    break;
+                                case '\n':
+                                    Writer.Write(new byte[] { 0x0A, 0x00 });
+                                    break;
+                                default:
+                                    Writer.Write(new byte[] {Convert.ToByte(Char), 0x00});
+                                    break;
+                            }
+
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Lithe/README.md
@@ -0,0 +1,18 @@
+# Lithe
+
+This program lets you edit the credits.dat file, it might be useful if you plan on making a mod package and want to mention people who helped.
+
+Run Options To Parse To Text:
+``
+Lithe.exe <Credits.dat>
+``
+
+Run Options To Parse Back To DAT:
+``
+Lithe.exe <Credits.dat>
+``
+
+Optional Argumens:
+``
+N/A
+``
