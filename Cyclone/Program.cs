ebfc3339aa0424df134980c99fc530436feb6836
Johnathon <f315729@trbvm.com>
2017-07-12 20:52:03 +1200

    eppic fail Tom
    Ughhh. ZIB file packing seems to be pretty hard since there is no start point. Or it's probably because I am stupid.

--- Cyclone/Program.cs
+++ Cyclone/Program.cs
@@ -1,11 +1,8 @@
-﻿using System;
+﻿using Celtic_Guardian;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.ComTypes;
-using System.Text;
-using System.Threading.Tasks;
-using Celtic_Guardian;
 
 namespace Cyclone
 {
@@ -17,30 +14,20 @@ namespace Cyclone
             Console.Title = "Cyclone";
             if (Args.Length <= 0)
                 Utilities.Log("Please Make Sure You Point Me Towards An Unpacked ZIB File.", Utilities.Event.Error, true, 1);
-            if (!File.Exists($"{Args[0]}\\.zib")) //To Stop Packing Random Files.
+            if (!File.Exists($"{Args[0]}\\.zib"))
                 Utilities.Log("Not an unpacked ZIB!", Utilities.Event.Error, true, 1);
 
             if (Args.Any(Arg => Arg == "-autovortex"))
             {
                 //Look For Vortex && YGO_DATA
             }
-            var ExtractedFolderName = Path.GetFileName(new FileInfo(Args[0]).Directory.Name).Replace("Unpacked_", string.Empty);
+            var ExtractedFolderName = Args[0].Replace("Unpacked_", string.Empty);
 
-            //File.Delete($"{Args[0]}\\.zib"); //Clean Up.
-            Utilities.CreateDummyFile(ExtractedFolderName, Utilities.DirSize(new DirectoryInfo(Args[0])));
-
-            foreach (var Item in Directory.GetFiles(Args[0]).Reverse()) //Reverse To Fill Images In, Then Offset Information. (HAS TO BE BETTER WAY D:)
+            //File.Delete($"{Args[0]}\\.zib");
+            var Writer = new BinaryWriter(File.Open(ExtractedFolderName, FileMode.Append, FileAccess.Write));
+            foreach (var Item in Directory.GetFiles(Args[0]))
             {
-                var FileInformation = new FileInfo(Item);
-                var FileSize = Utilities.DecToHex(FileInformation.Length.ToString());
-                var FileName = FileInformation.Name;
-                var FileOffset = 0x0; //HEXify, and Figure out
-                var Reader = new BinaryReader();
-                
-
-
-
-
+                //Write Pack Logic.
             }
         }
     }
