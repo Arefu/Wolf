e6b50a2b0a7bcdc44ace14da670840c82dedef53
Johnathon <f315729@trbvm.com>
2017-12-02 15:13:26 +1300

    Who has two thumbs and remembers the rudiments of his linear algebra courses? Apparently, this guy.
    Cyclone will now repack cardcropHD400 correctly.

--- Cyclone/Program.cs
+++ Cyclone/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,17 +7,14 @@ using System.Text;
 using System.Windows.Forms;
 using Celtic_Guardian;
 
-namespace Cyclone
+namespace YGOPRODraft
 {
-    internal class Program
+    public class Cyclone
     {
-        public static List<PackData> Data = new List<PackData>();
-
         [STAThread]
         private static void Main()
         {
-            Console.Title = "Cyclone";
-
+            //Load ZIB.
             var ZibFolder = "";
             using (var Fbd = new FolderBrowserDialog())
             {
@@ -30,37 +28,54 @@ namespace Cyclone
                     ZibFolder = Fbd.SelectedPath;
             }
 
-            var FileNamesToReadInOrder = new List<string>();
-            if (File.Exists($"{ZibFolder}\\Index.zib"))
-                FileNamesToReadInOrder = File.ReadAllLines($"{ZibFolder}\\Index.zib").ToList();
+            if (!File.Exists($"{ZibFolder}\\Index.zib"))
+                throw new Exception("Index.ZIB Not Found; Not Unpacked Correctly!");
 
-            using (var Writer = new BinaryWriter(File.Open(ZibFolder.Replace("Unpacked", ""), FileMode.OpenOrCreate, FileAccess.Write)))
+            var FileNamesToReadInOrder = File.ReadAllLines($"{ZibFolder}\\Index.zib").ToList();
+
+            using (var Writer = new BinaryWriter(File.Open(ZibFolder.Replace(" Unpacked", string.Empty), FileMode.OpenOrCreate, FileAccess.Write)))
             {
-                var CurrentOffset = (Directory.GetFiles(ZibFolder).Length - 1) * 64 + 16; //First should be Number of Files * 64 + 16.
+                var CurrentOffset = (uint)(Directory.GetFiles(ZibFolder).Length - 1) * 64 + 16; //First should be Number of Files * 64 + 16.
                 foreach (var FileToPack in FileNamesToReadInOrder)
                 {
-                    var OffSet = CurrentOffset;
-                    var CurrentFileSize = new FileInfo($"{ZibFolder}\\{FileToPack}").Length.ToString();
-                    CurrentOffset += Convert.ToInt32(CurrentFileSize);
+                    var CurrentFileSize = new FileInfo($"{ZibFolder}\\{FileToPack}").Length;
 
-                    Writer.Write(0x00);
-                    Writer.Write(SwapBytes(OffSet));
-                    Writer.Write(0x00);
-                    Writer.Write(SwapBytes((int)new FileInfo($"{ZibFolder}\\{FileToPack}").Length));
+                    //File OffSet.
+                    Writer.Write(new byte[4]);
+                    Writer.Write(SwapBytes(CurrentOffset));
 
+                    //File Size.
+                    Writer.Write(new byte[4]);
+                    Writer.Write(SwapBytes((uint)new FileInfo($"{ZibFolder}\\{FileToPack}").Length));
+
+                    //File Name.
                     Writer.Write(Encoding.ASCII.GetBytes(FileToPack));
-                    Writer.Write(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 });
+                    Writer.Write(FileToPack.Length == 9 ? new byte[39] : new byte[40]);
 
+                    CurrentOffset += Convert.ToUInt32(16 * ((CurrentFileSize + 15) / 16));
                 }
+
+                ////Also add 16 bytes of padding here.
+                Writer.Write(new byte[16]);
+
                 foreach (var FileToPack in FileNamesToReadInOrder)
-                    Writer.Write(File.ReadAllBytes($"{ZibFolder}\\{FileToPack}"));
+                {
+                    byte[] bytes = File.ReadAllBytes($"{ZibFolder}\\{FileToPack}");
+                    int numBytesWithPadding = 16 * ((bytes.Length + 15) / 16);
+                    Writer.Write(bytes);
+
+                    //Pad to multiples of 16 bytes, aligned.
+                   var fill = new byte[numBytesWithPadding - bytes.Length];
+                    Writer.Write(fill);
+                    }
+
+                //TODO: need to add padding to 16 bytes
             }
         }
-
-        public static int SwapBytes(int Bytes)
+        public static uint SwapBytes(uint x)
         {
-            Bytes = (Bytes >> 16) | (Bytes << 16);
-            return (int)((Bytes & 0xFF00FF00) >> 8) | ((Bytes & 0x00FF00FF) << 8);
+            x = (x >> 16) | (x << 16);
+            return ((x & 0xFF00FF00) >> 8) | ((x & 0x00FF00FF) << 8);
         }
     }
 }
---
+++ Elroy/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Elroy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FileLoadedStatusLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.FileLoadedStatusLabel);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(487, 329);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(125, 100);
+            this.panel1.TabIndex = 0;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel1_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 98);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Drag Your Save Here";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FileLoadedStatusLabel
+            // 
+            this.FileLoadedStatusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.FileLoadedStatusLabel.ForeColor = System.Drawing.Color.Red;
+            this.FileLoadedStatusLabel.Location = new System.Drawing.Point(3, 57);
+            this.FileLoadedStatusLabel.Name = "FileLoadedStatusLabel";
+            this.FileLoadedStatusLabel.Size = new System.Drawing.Size(121, 42);
+            this.FileLoadedStatusLabel.TabIndex = 1;
+            this.FileLoadedStatusLabel.Text = "Not Loaded";
+            this.FileLoadedStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(640, 480);
+            this.MinimumSize = new System.Drawing.Size(640, 480);
+            this.Name = "Form1";
+            this.Text = "Elroy";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label FileLoadedStatusLabel;
+    }
+}
+
