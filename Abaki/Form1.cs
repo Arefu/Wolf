b4aec0729d9c6669746c34ed7c6bdcffec03a5a4
Johnathon <f315729@trbvm.com>
2017-12-28 21:42:46 +1300

    (c) Microsoft 1988

--- Abaki/Form1.cs
+++ Abaki/Form1.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Celtic_Guardian;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Windows.Forms;
 
 namespace Abaki
@@ -11,35 +13,77 @@ namespace Abaki
         {
             InitializeComponent();
         }
+        string LanguageFile = "";
 
         private void LoadToolStripMenuItem_Click(object Sender, EventArgs Args)
         {
+            listBox1.Items.Clear();
+
             using (var Ofd = new OpenFileDialog())
             {
                 Ofd.Title = "Select File To Decode";
                 Ofd.Filter = "Language file (*.bnd) | *.bnd";
                 if (Ofd.ShowDialog() != DialogResult.OK) return;
 
-                if (!Directory.Exists(new FileInfo(Ofd.FileName).Name))
-                    Directory.CreateDirectory(new FileInfo(Ofd.FileName).Name);
-
-                var LangFileName = new FileInfo(Ofd.FileName).Name;
+                LanguageFile = Ofd.SafeFileName;
                 using (var Reader = new BinaryReader(File.Open(Ofd.FileName, FileMode.Open, FileAccess.Read)))
                 {
-                    const long DataStartOffset = 0x11CD;
-                    const long AmountOfStrings = 0x472;
-                    var ListOfStringOffSets = new List<int>();
+                    var AmountOfStrings = Convert.ToInt32(Utilities.HexToDec(Utilities.ByteArrayToString(Reader.ReadBytes(4)).TrimStart('0'))) - 1;
+                    var JumpTo = Convert.ToInt32(Utilities.HexToDec(Utilities.ByteArrayToString(Reader.ReadBytes(4)).TrimStart('0')));
+                    var CurrentPos = Reader.BaseStream.Position;
+                    Reader.BaseStream.Position = JumpTo - 4;
+                    var LastOffset = Convert.ToInt32(Utilities.HexToDec(Utilities.ByteArrayToString(Reader.ReadBytes(4)).TrimStart('0')));
+                    Reader.BaseStream.Position = CurrentPos;
+
+                    var Count = 0;
+                    var StringOffsets = new List<int>();
+                    do
+                    {
+                        StringOffsets.Add(Convert.ToInt32(Utilities.HexToDec(Utilities.ByteArrayToString(Reader.ReadBytes(4)).TrimStart('0'))));
+                        Count++;
+                    } while (Count < AmountOfStrings);
+
+                    for (Count = 0; Count < StringOffsets.Count; Count++)
+                    {
+                        if (Count != StringOffsets.Count - 1)
+                        {
+                            if ((int)Reader.BaseStream.Position > StringOffsets[Count + 1])
+                                listBox1.Items.Add(Utilities.GetText(Reader.ReadBytes((int)Reader.BaseStream.Position - StringOffsets[Count + 1])));
+                            else
+                                listBox1.Items.Add(Utilities.GetText(Reader.ReadBytes(StringOffsets[Count] - (int)Reader.BaseStream.Position)));
+                        }
+                        else
+                            listBox1.Items.Add(Utilities.GetText(Reader.ReadBytes((int)Reader.BaseStream.Length - (int)Reader.BaseStream.Position)));
+                    }
                 }
             }
         }
 
         private void ExportToolStripMenuItem_Click(object Sender, EventArgs Args)
         {
+            using (var Writer = new BinaryWriter(File.Open(LanguageFile, FileMode.OpenOrCreate, FileAccess.Write)))
+            {
+                Writer.Write(new byte[] { 0x00, 0x00});
+                Writer.Write(Encoding.Default.GetBytes(listBox1.Items.Count.ToString()));
+
+                foreach (var Item in listBox1.Items)
+                {
+                    foreach (var Char in Item.ToString())
+                    {
+                        Writer.Write(new byte[] { Convert.ToByte(Char), 0x00 });
+                    }
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             listBox1.Items[listBox1.SelectedIndex] = textBox1.Text;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = listBox1.GetItemText(listBox1.SelectedItem);
         }
     }
 }
--- Wolf/ContextMenuFunctions.cs
+++ Wolf/ContextMenuFunctions.cs
@@ -1,62 +1,4 @@
-<<<<<<< HEAD
-﻿using Celtic_Guardian;
 using System;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Wolf
-{
-    public static class ContextMenuFunctions
-    {
-        public static void ExtractFile(ListViewItem Item, string ExportPath = "")
-        {
-            Task.Run(() =>
-            {
-
-                var FileToExport = Form1.Data.First(File => File.Item3.Contains(Item.Text));
-
-                Directory.CreateDirectory("Exported");
-                var BytesToRead = 0L;
-                foreach (var File in Form1.Data)
-                {
-                    if (File.Item3 == FileToExport.Item3)
-                        break;
-
-                    var AligneSize = Utilities.IsAligned(File.Item1);
-                    BytesToRead = AligneSize + BytesToRead;
-
-                    if (File.Item3 == FileToExport.Item3)
-                        break;
-                }
-                using (var BReader = new BinaryReader(File.Open($"{Form1.InstallDir}\\YGO_DATA.dat", FileMode.Open,
-                    FileAccess.Read)))
-                {
-                    if (ExportPath == "")
-                        ExportPath = FileToExport.Item3;
-
-                    new FileInfo("YGO_DATA\\" + FileToExport.Item3).Directory?.Create();
-                    using (var Writer = new BinaryWriter(File.Open("YGO_DATA\\" + ExportPath, FileMode.Create, FileAccess.Write)))
-                    {
-                        BReader.BaseStream.Position = BytesToRead;
-                        Writer.Write(BReader.ReadBytes(FileToExport.Item1));
-                        BReader.Dispose();
-                    }
-                }
-            });
-        }
-
-        public static void ViewImage(ListViewItem Item)
-        {
-            var RandFileName = new Random(DateTime.Now.Millisecond).Next();
-            ExtractFile(Item, $"{Path.GetTempPath()}\\{RandFileName}");
-            var Viewer = new ImageViewer($"{Path.GetTempPath()}\\{RandFileName}");
-            Viewer.ShowDialog();
-        }
-    }
-=======
-﻿using System;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
@@ -68,7 +10,7 @@ namespace Wolf
         public static void ExtractFile(ListViewItem Item, string ExportPath = "")
         {
             var FileToExport = Form1.Data.First(File => File.Item3.Contains(Item.Text));
-            
+
             var BytesToRead = 0L;
             foreach (var File in Form1.Data)
             {
@@ -107,5 +49,4 @@ namespace Wolf
             Viewer.ShowDialog();
         }
     }
->>>>>>> master
 }
