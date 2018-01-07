37916610b9950c185ceaf06dee12ce344734f9b2
Johnathon <f315729@trbvm.com>
2018-01-07 13:23:15 +1300

    Copy-paste to fix previous copy-paste
    Finally, the strings can be manipulated and translated, although technical info might be two byte wide letters will fail due to my shit code :D

--- Abaki/Form1.Designer.cs
+++ Abaki/Form1.Designer.cs
@@ -39,10 +39,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -89,8 +85,9 @@
             // quitToolStripMenuItem
             // 
             this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
-            this.quitToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
             // 
             // listBox1
             // 
@@ -136,50 +133,11 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(360, 121);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Search";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(360, 163);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(171, 44);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Search";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(360, 137);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(174, 20);
-            this.textBox2.TabIndex = 8;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(407, 120);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(86, 17);
-            this.checkBox1.TabIndex = 9;
-            this.checkBox1.Text = "Exact Match";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(546, 681);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -187,8 +145,9 @@
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Abaki";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -209,10 +168,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Abaki/Form1.cs
+++ Abaki/Form1.cs
@@ -1,9 +1,7 @@
 ﻿using Celtic_Guardian;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Windows.Forms;
 
@@ -46,16 +44,7 @@ namespace Abaki
 
                     for (Count = 0; Count < StringOffsets.Count; Count++)
                     {
-                        if (Count != StringOffsets.Count - 1)
-                            //Un-Comment Once RePacking Logic Is Done.
-                            //{
-                            //    if ((int)Reader.BaseStream.Position > StringOffsets[Count + 1])
-                            //        listBox1.Items.Add(Utilities.GetText(Reader.ReadBytes((int)Reader.BaseStream.Position - StringOffsets[Count + 1])));
-                            //    else
-                            listBox1.Items.Add(System.Text.Encoding.BigEndianUnicode.GetString(Reader.ReadBytes(StringOffsets[Count] - (int)Reader.BaseStream.Position)));
-                        //}
-                        else
-                            listBox1.Items.Add(System.Text.Encoding.BigEndianUnicode.GetString(Reader.ReadBytes((int)Reader.BaseStream.Length - (int)Reader.BaseStream.Position)));
+                        listBox1.Items.Add(Count != StringOffsets.Count - 1 ? System.Text.Encoding.BigEndianUnicode.GetString(Reader.ReadBytes(StringOffsets[Count] - (int) Reader.BaseStream.Position)) : System.Text.Encoding.BigEndianUnicode.GetString(Reader.ReadBytes((int) Reader.BaseStream.Length - (int) Reader.BaseStream.Position)));
                     }
                 }
             }
@@ -64,23 +53,22 @@ namespace Abaki
         private void ExportToolStripMenuItem_Click(object Sender, EventArgs Args)
         {
             if (listBox1.Items.Count == 0)
-                return;
+                MessageBox.Show("There is nothing to export.", "Nothing To Export!", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-            using (var Writer = new BinaryWriter(File.Open(LanguageFile, FileMode.OpenOrCreate, FileAccess.Write)))
+            using (var Writer = new BinaryWriter(File.Open(LanguageFile, FileMode.Create, FileAccess.Write)))
             {
-                Writer.Write(BitConverter.GetBytes(Utilities.SwapBytes((uint)listBox1.Items.Count + 1))); //Might Not Need To Be + 1?
+                Writer.Write(BitConverter.GetBytes(Utilities.SwapBytes((uint)listBox1.Items.Count + 1)));
 
-                long Sum = listBox1.Items.Count * 4;
-                //LanguageParse(Encoding.BigEndianUnicode.GetBytes(Item.ToString()));
+                long Sum = listBox1.Items.Count * 4 + 4;
                 foreach (var Item in listBox1.Items)
                 {
-                    var Offset = 0L;
-                    Sum = Sum + LanguageParse(Encoding.BigEndianUnicode.GetBytes(Item.ToString()));
                     Writer.Write(Utilities.SwapBytes((uint)Sum));
-                    Offset = Writer.BaseStream.Position;
-                    Writer.BaseStream.Position = Sum;
+                    Sum = Sum + LanguageParse(Encoding.BigEndianUnicode.GetBytes(Item.ToString()));
+                }
+
+                foreach (var Item in listBox1.Items)
+                {
                     Writer.Write(Encoding.BigEndianUnicode.GetBytes(Item.ToString()));
-                    Writer.BaseStream.Position = Offset;
                 }
             }
         }
@@ -91,20 +79,13 @@ namespace Abaki
             var HexString = new StringBuilder(Bytes.Length * 2);
             foreach (var Byte in Bytes)
             {
-                if (Byte == 0x00)
-                {
-                    HexString.Append('.');
-                }
-                else
-                {
-                    HexString.Append(Convert.ToChar(Byte));
-                }
+                HexString.Append(Byte == 0x00 ? '.' : Convert.ToChar(Byte));
             }
 
             return HexString.ToString().Length;
         }
 
-        private void Button1_Click(object sender, EventArgs e)
+        private void Button1_Click(object Sender, EventArgs Args)
         {
             if (textBox1.Text.Length == 0)
                 listBox1.Items[listBox1.SelectedIndex] = "";
@@ -112,9 +93,14 @@ namespace Abaki
             listBox1.Items[listBox1.SelectedIndex] = textBox1.Text;
         }
 
-        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void ListBox1_SelectedIndexChanged(object Sender, EventArgs Args)
         {
             textBox1.Text = listBox1.GetItemText(listBox1.SelectedItem);
+        }
+
+        private void quitToolStripMenuItem_Click(object Sender, EventArgs Args)
+        {
+            Application.Exit();
         }
     }
 }
