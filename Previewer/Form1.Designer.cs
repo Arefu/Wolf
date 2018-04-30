24b82534ca305da3447e7d6fa38954c862b280a0
Johnathon <f315729@trbvm.com>
2018-04-30 23:36:07 +1200

    pointless limitation
    So, the DLC flag checking might take longer as it's nto where me or Sharp thought it was sooo... But moving on we're making "gains"

--- Previewer/Form1.Designer.cs
+++ Previewer/Form1.Designer.cs
@@ -62,13 +62,13 @@
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
-            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
@@ -86,7 +86,7 @@
             this.exitToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(510, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(562, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -181,20 +181,21 @@
             // 
             this.cardTitle.Location = new System.Drawing.Point(6, 19);
             this.cardTitle.Name = "cardTitle";
-            this.cardTitle.Size = new System.Drawing.Size(270, 20);
+            this.cardTitle.Size = new System.Drawing.Size(304, 20);
             this.cardTitle.TabIndex = 1;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(9, 66);
+            this.pictureBox1.Location = new System.Drawing.Point(6, 45);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(267, 201);
+            this.pictureBox1.Size = new System.Drawing.Size(304, 304);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // AtkTextBox
             // 
-            this.AtkTextBox.Location = new System.Drawing.Point(40, 349);
+            this.AtkTextBox.Location = new System.Drawing.Point(40, 500);
             this.AtkTextBox.Name = "AtkTextBox";
             this.AtkTextBox.Size = new System.Drawing.Size(100, 20);
             this.AtkTextBox.TabIndex = 4;
@@ -202,7 +203,7 @@
             // 
             // DefTextBox
             // 
-            this.DefTextBox.Location = new System.Drawing.Point(180, 349);
+            this.DefTextBox.Location = new System.Drawing.Point(210, 500);
             this.DefTextBox.Name = "DefTextBox";
             this.DefTextBox.Size = new System.Drawing.Size(100, 20);
             this.DefTextBox.TabIndex = 5;
@@ -227,23 +228,23 @@
             this.groupBox1.Controls.Add(this.DefTextBox);
             this.groupBox1.Location = new System.Drawing.Point(12, 27);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(282, 375);
+            this.groupBox1.Size = new System.Drawing.Size(326, 542);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Card Information";
             // 
             // cardDesc
             // 
-            this.cardDesc.Location = new System.Drawing.Point(9, 269);
+            this.cardDesc.Location = new System.Drawing.Point(6, 355);
             this.cardDesc.Name = "cardDesc";
-            this.cardDesc.Size = new System.Drawing.Size(267, 74);
+            this.cardDesc.Size = new System.Drawing.Size(304, 139);
             this.cardDesc.TabIndex = 15;
             this.cardDesc.Text = "";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 352);
+            this.label3.Location = new System.Drawing.Point(6, 503);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(31, 13);
             this.label3.TabIndex = 7;
@@ -252,7 +253,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(146, 352);
+            this.label2.Location = new System.Drawing.Point(176, 503);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(31, 13);
             this.label2.TabIndex = 6;
@@ -314,7 +315,7 @@
             this.groupBox2.Controls.Add(this.prevCard);
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Location = new System.Drawing.Point(300, 29);
+            this.groupBox2.Location = new System.Drawing.Point(344, 29);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(200, 92);
             this.groupBox2.TabIndex = 13;
@@ -325,7 +326,7 @@
             // 
             this.groupBox3.Controls.Add(this.numericUpDown1);
             this.groupBox3.Controls.Add(this.button1);
-            this.groupBox3.Location = new System.Drawing.Point(300, 127);
+            this.groupBox3.Location = new System.Drawing.Point(344, 127);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(119, 75);
             this.groupBox3.TabIndex = 14;
@@ -371,22 +372,61 @@
             this.groupBox4.Controls.Add(this.comboBox1);
             this.groupBox4.Controls.Add(this.textBox1);
             this.groupBox4.Controls.Add(this.checkBox1);
-            this.groupBox4.Location = new System.Drawing.Point(300, 208);
+            this.groupBox4.Location = new System.Drawing.Point(344, 208);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Size = new System.Drawing.Size(200, 194);
             this.groupBox4.TabIndex = 16;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Card Editing";
             // 
-            // textBox1
+            // linkLabel4
             // 
-            this.textBox1.ForeColor = System.Drawing.Color.Gray;
-            this.textBox1.Location = new System.Drawing.Point(6, 32);
-            this.textBox1.MaxLength = 8;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(188, 20);
-            this.textBox1.TabIndex = 16;
-            this.textBox1.Text = "Image ID";
+            this.linkLabel4.AutoSize = true;
+            this.linkLabel4.Location = new System.Drawing.Point(3, 143);
+            this.linkLabel4.Name = "linkLabel4";
+            this.linkLabel4.Size = new System.Drawing.Size(74, 13);
+            this.linkLabel4.TabIndex = 24;
+            this.linkLabel4.TabStop = true;
+            this.linkLabel4.Text = "Miscellaneous";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(6, 115);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(188, 21);
+            this.comboBox2.TabIndex = 23;
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(3, 99);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(71, 13);
+            this.linkLabel3.TabIndex = 22;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "Card Attribute";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(3, 59);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(59, 13);
+            this.linkLabel2.TabIndex = 21;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Card Type:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(3, 16);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(53, 13);
+            this.linkLabel1.TabIndex = 20;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Image ID:";
             // 
             // comboBox1
             // 
@@ -400,60 +440,21 @@
             this.comboBox1.Size = new System.Drawing.Size(188, 21);
             this.comboBox1.TabIndex = 17;
             // 
-            // linkLabel1
+            // textBox1
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(3, 16);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(53, 13);
-            this.linkLabel1.TabIndex = 20;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "Image ID:";
-            // 
-            // linkLabel2
-            // 
-            this.linkLabel2.AutoSize = true;
-            this.linkLabel2.Location = new System.Drawing.Point(3, 59);
-            this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(59, 13);
-            this.linkLabel2.TabIndex = 21;
-            this.linkLabel2.TabStop = true;
-            this.linkLabel2.Text = "Card Type:";
-            // 
-            // linkLabel3
-            // 
-            this.linkLabel3.AutoSize = true;
-            this.linkLabel3.Location = new System.Drawing.Point(3, 99);
-            this.linkLabel3.Name = "linkLabel3";
-            this.linkLabel3.Size = new System.Drawing.Size(71, 13);
-            this.linkLabel3.TabIndex = 22;
-            this.linkLabel3.TabStop = true;
-            this.linkLabel3.Text = "Card Attribute";
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(6, 115);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(188, 21);
-            this.comboBox2.TabIndex = 23;
-            // 
-            // linkLabel4
-            // 
-            this.linkLabel4.AutoSize = true;
-            this.linkLabel4.Location = new System.Drawing.Point(3, 143);
-            this.linkLabel4.Name = "linkLabel4";
-            this.linkLabel4.Size = new System.Drawing.Size(74, 13);
-            this.linkLabel4.TabIndex = 24;
-            this.linkLabel4.TabStop = true;
-            this.linkLabel4.Text = "Miscellaneous";
+            this.textBox1.ForeColor = System.Drawing.Color.Gray;
+            this.textBox1.Location = new System.Drawing.Point(6, 32);
+            this.textBox1.MaxLength = 8;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 20);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.Text = "Image ID";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(510, 416);
+            this.ClientSize = new System.Drawing.Size(562, 576);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
--- Previewer/Form1.cs
+++ Previewer/Form1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
 using Celtic_Guardian;
@@ -12,7 +12,9 @@ namespace Previewer
         private readonly List<int> DescIndx = new List<int>();
         private readonly List<int> NameIndx = new List<int>();
         private char CurrentLanguage = 'E'; //Default To English.
+        private string CurrentUnpackedImagesDir = String.Empty;
         private int GlobalIndex;
+        internal static BinaryReader CardPropReader = new BinaryReader(File.Open($"YGO_DATA/bin/CARD_Prop.bin", FileMode.Open, FileAccess.Read));
 
         public Form1()
         {
@@ -23,7 +25,6 @@ namespace Previewer
         private void Form1_Load(object Sender, EventArgs Args)
         {
             if (Directory.Exists("YGO_DATA")) return;
-
             MessageBox.Show(@"YGO_DATA Not Found! Some Things Might Be A Tad Broken.\nRefer To Wiki For More Info.", @"YGO_DATA Missing!", MessageBoxButtons.OK, MessageBoxIcon.Information);
             Application.Exit();
         }
@@ -35,6 +36,21 @@ namespace Previewer
 
         private void CardIndexToolStripMenuItem_Click(object Sender, EventArgs Args)
         {
+            if (CurrentUnpackedImagesDir == string.Empty)
+            {
+                using (var FolderSelect = new FolderBrowserDialog())
+                {
+                    FolderSelect.Description = @"Please Select Your 'cardcropHD4XX.jpg.zib Unpacked' Folder!";
+                    FolderSelect.SelectedPath = Application.StartupPath;
+                    var Reply = FolderSelect.ShowDialog();
+                    if (Reply != DialogResult.OK) return;
+
+                    CurrentUnpackedImagesDir = FolderSelect.SelectedPath;
+                }
+            }
+
+            CardPropReader.BaseStream.Position += 0x8;
+
             using (var FileDialog = new OpenFileDialog())
             {
                 FileDialog.Title = @"Select Card Index File";
@@ -62,29 +78,70 @@ namespace Previewer
             numericUpDown1.Minimum = 1;
         }
 
+        struct CardTemplate
+        {
+            internal uint Atk;
+            internal uint Def;
+            internal uint Level;
+            internal short Id;
+        };
+
         private void LoadCards()
         {
-            var Index = GlobalIndex; //Dunno Why?
             using (var CardTitleReader = new BinaryReader(File.Open($"YGO_DATA/bin/CARD_Name_{CurrentLanguage}.bin", FileMode.Open, FileAccess.Read)))
             {
                 using (var CardDescReader = new BinaryReader(File.Open($"YGO_DATA/bin/CARD_Desc_{CurrentLanguage}.bin", FileMode.Open, FileAccess.Read)))
                 {
-                    CardTitleReader.BaseStream.Position = NameIndx[Index];
-                    CardDescReader.BaseStream.Position = DescIndx[Index];
+                    CardTitleReader.BaseStream.Position = NameIndx[GlobalIndex];
+                    CardDescReader.BaseStream.Position = DescIndx[GlobalIndex];
+                    var Offset = GlobalIndex * 8;
+                    if (Offset == 0)
+                        Offset = 8;
+                    else
+                        CardPropReader.BaseStream.Position = Offset;
+
+                    CardPropReader.BaseStream.Position = Offset;
+
                     if (CardTitleReader.BaseStream.Position < CardTitleReader.BaseStream.Length)
                     {
                         if (CardDescReader.BaseStream.Position < CardDescReader.BaseStream.Length)
                         {
-                            var NameSum = NameIndx[Index + 1] - NameIndx[Index];
-                            var DescSum = DescIndx[Index + 1] - DescIndx[Index];
+                            var NameSum = NameIndx[GlobalIndex + 1] - NameIndx[GlobalIndex];
+                            var DescSum = DescIndx[GlobalIndex + 1] - DescIndx[GlobalIndex];
+
+
+                            var Card = ByteJumbo(CardPropReader.ReadUInt32(), CardPropReader.ReadUInt32());
+
                             cardTitle.Text = Utilities.GetText(CardTitleReader.ReadBytes(NameSum));
                             cardDesc.Text = Utilities.GetText(CardDescReader.ReadBytes(DescSum));
+                            pictureBox1.Image = Image.FromFile($"{CurrentUnpackedImagesDir}/{Card.Id}.jpg");
+                            AtkTextBox.Text = Card.Atk.ToString();
+                            DefTextBox.Text = Card.Def.ToString();
+                            textBox1.Text = Card.Id.ToString();
                         }
-                    }   
+                    }
                 }
             }
 
-            label5.Text = Index.ToString();
+            label5.Text = GlobalIndex.ToString();
+        }
+
+        private static CardTemplate ByteJumbo(uint FirstChunk, uint SecondChunk)
+        {
+            var First = (FirstChunk << 18) | ((FirstChunk & 0x7FC000 | FirstChunk >> 18) >> 5);
+            var Second = (((SecondChunk & 1u) | (SecondChunk << 21)) & 0x80000001 | ((SecondChunk & 0x7800) | ((SecondChunk & 0x780 | ((SecondChunk & 0x7E) << 10)) << 8)) << 6 | ((SecondChunk & 0x38000 | ((SecondChunk & 0x7C0000 | ((SecondChunk & 0x7800000 | (SecondChunk >> 8) & 0x780000) >> 9)) >> 8)) >> 1));
+
+            var CardId = (short)((First >> 18) & 0x3FFF);
+            var Atk = (First >> 9) & 0x1FF;
+            var Def = First & 0x1FF;
+            var Level = (Second >> 17) & 0xF;
+
+            CardTemplate Card;
+            Card.Id = CardId;
+            Card.Atk = Atk*10;
+            Card.Def = Def*10;
+            Card.Level = Level;
+            return Card;
         }
 
         private void DeckToolStripMenuItem_Click(object Sender, EventArgs Args)
@@ -96,8 +153,8 @@ namespace Previewer
             foreach (ToolStripMenuItem Language in languageToolStripMenuItem.DropDownItems)
                 Language.Checked = false;
 
-            ((ToolStripMenuItem) Sender).Checked = true;
-            CurrentLanguage = ((ToolStripMenuItem) Sender).Text[0];
+            ((ToolStripMenuItem)Sender).Checked = true;
+            CurrentLanguage = ((ToolStripMenuItem)Sender).Text[0];
         }
 
         private void Button1_Click(object Sender, EventArgs Args)
@@ -120,6 +177,7 @@ namespace Previewer
             {
                 GlobalIndex--;
                 numericUpDown1.Value--;
+                CardPropReader.BaseStream.Position -= 16;
                 LoadCards();
             }
             else
@@ -130,7 +188,7 @@ namespace Previewer
 
         private void Button1_Click_1(object Sender, EventArgs Args)
         {
-            GlobalIndex = (int) numericUpDown1.Value;
+            GlobalIndex = (int)numericUpDown1.Value;
             LoadCards();
         }
 
